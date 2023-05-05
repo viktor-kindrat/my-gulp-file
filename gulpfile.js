de4a949e9af2f46305aef15d1117a6155baa9527
Viktor Kindrat <84918090+viktor-kindrat@users.noreply.github.com>
2023-05-05 22:12:49 +0300

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ const sass = require('gulp-sass')(require('sass'));
 const webp = require('gulp-webp');
 
 function compileStyles() {
-    return gulp.src('./src/**/*.scss')
+    return gulp.src('./**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/'));
+        .pipe(gulp.dest('./'));
 }
 
 function convertImg() {
-    return gulp.src(['./src/**/*.jpg', './src/**/*.png', './src/**/*.jpeg', './src/**/*.'])
+    return gulp.src(['./**/*.jpg', './**/*.png', './**/*.jpeg'])
         .pipe(webp())
-        .pipe(gulp.dest('./src/'))
+        .pipe(gulp.dest('./'))
 }
 
 exports.compileStyles = compileStyles;
 
 exports.watchSass = function() {
-    gulp.watch('./src/**/*.scss', gulp.series('compileStyles'));
+    gulp.watch('./**/*.scss', gulp.series('compileStyles'));
 };
 exports.imgConvert = convertImg
